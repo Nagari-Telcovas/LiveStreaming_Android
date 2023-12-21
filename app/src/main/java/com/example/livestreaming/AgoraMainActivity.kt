decf78eb1f65de1c23f71e416249f646c3e77096
dheeraj92 <dheeraj.varshney@telcovasworld.com>
2023-12-21 14:11:32 +0530

    Add Permissions

--- app/src/main/java/com/example/livestreaming/AgoraMainActivity.kt
+++ app/src/main/java/com/example/livestreaming/AgoraMainActivity.kt
@@ -1,59 +1,167 @@
-package com.example.livestreaming;
+package com.example.livestreaming
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.ArrayList
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
+class AgoraMainActivity : AppCompatActivity() {
+    var channelProfile = 0
+    var cameraGranted = false
+    var permissionsStr = arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+    var permissionsCount = 0
+    lateinit var permissionsList: ArrayList<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agora_main)
+        //checkPermissionCamera()
+        permissionsList = ArrayList()
+        permissionsList.addAll(permissionsStr)
+        askForPermissions(permissionsList)
 
-import io.agora.rtc2.Constants;
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val host = findViewById<RadioButton>(R.id.host)
+        val audience = findViewById<RadioButton>(R.id.audience)
+        val channelName = findViewById<EditText>(R.id.channelName)
+        val submit = findViewById<Button>(R.id.submit)
 
-public class AgoraMainActivity extends AppCompatActivity {
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val radio: RadioButton = findViewById(checkedId)
+            Toast.makeText(applicationContext, " On checked change :" + " ${radio.text}", Toast.LENGTH_SHORT).show()
+        }
 
-    int channelProfile;
-    public static final String channelMessage = "com.agora.samtan.agorabroadcast.CHANNEL";
-    public static final String profileMessage = "com.agora.samtan.agorabroadcast.PROFILE";
+        submit.setOnClickListener{
+           // if (cameraGranted){
+                var id: Int = radioGroup.checkedRadioButtonId
+                if (id!=-1){
+                    val channelName = channelName.text.toString()
+                    val intent = Intent(this, AgoraVideoActivity::class.java)
+                    intent.putExtra(channelMessage, channelName)
+                    intent.putExtra(profileMessage, channelProfile)
+                    startActivity(intent)
+                   // val radio:RadioButton = findViewById(id)
+                  //  Toast.makeText(applicationContext,"On button click :" + " ${radio.text}", Toast.LENGTH_SHORT).show()
+                }else{
+                    // If no radio button checked in this radio group
+                    Toast.makeText(applicationContext,"Please Select one Item", Toast.LENGTH_SHORT).show()
+             //   }
+            //}else{
+                //CommonMethods.showMessage(this, getString(R.string.permission_required))
+            }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agora_main);
-
-        int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+        }
+        val MY_PERMISSIONS_REQUEST_CAMERA = 0
         // Here, this is the current activity
-     /*   if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+        /*   if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_CAMERA);
 
         }*/
     }
 
-    public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
-        switch (view.getId()) {
-            case R.id.host:
-                if (checked) {
-                    channelProfile = Constants.CLIENT_ROLE_BROADCASTER;
+
+
+/*    fun onRadioButtonClicked(view: View) {
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.host -> if (checked) {
+                channelProfile = Constants.CLIENT_ROLE_BROADCASTER
+            }
+
+            R.id.audience -> if (checked) {
+                channelProfile = Constants.CLIENT_ROLE_AUDIENCE
+            }
+        }
+    }*/
+
+    /*fun onSubmit(view: View?) {
+       // val channel = findViewById<View>(R.id.channel) as EditText
+        val channelName = channel.text.toString()
+        val intent = Intent(this, AgoraVideoActivity::class.java)
+        intent.putExtra(channelMessage, channelName)
+        intent.putExtra(profileMessage, channelProfile)
+        startActivity(intent)
+    }*/
+
+    companion object {
+        const val channelMessage = "com.agora.samtan.agorabroadcast.CHANNEL"
+        const val profileMessage = "com.agora.samtan.agorabroadcast.PROFILE"
+    }
+
+    //-- Permission
+    private var permissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        val list = ArrayList(result.values)
+        permissionsList = ArrayList()
+        permissionsCount = 0
+        for (i in list.indices) {
+            if (shouldShowRequestPermissionRationale(permissionsStr[i])) {
+                permissionsList.add(permissionsStr[i])
+            } else if (!hasPermission(this, permissionsStr[i])) {
+                if (permissionsStr[i] === Manifest.permission.CAMERA) {
+                    CommonMethods.showMessage(this, "Please allow Camera permission!")
                 }
-                break;
-            case R.id.audience:
-                if (checked) {
-                    channelProfile = Constants.CLIENT_ROLE_AUDIENCE;
+                if (permissionsStr[i] === Manifest.permission.RECORD_AUDIO) {
+                 //   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                        CommonMethods.showMessage(this, "Please allow MicroPhone permission!")
+                 //   }
                 }
-                break;
+                permissionsCount++
+            }
+        }
+        if (permissionsList.size > 0) {
+            askForPermissions(permissionsList)
         }
     }
 
-    public void onSubmit(View view) {
-        EditText channel = (EditText) findViewById(R.id.channel);
-        String channelName = channel.getText().toString();
-        Intent intent = new Intent(this, AgoraVideoActivity.class);
-        intent.putExtra(channelMessage, channelName);
-        intent.putExtra(profileMessage, channelProfile);
-        startActivity(intent);
+    private fun hasPermission(context: Context, permissionStr: String): Boolean {
+
+        return ContextCompat.checkSelfPermission(context, permissionStr) == PackageManager.PERMISSION_GRANTED
     }
+    private fun askForPermissions(permissionsList: ArrayList<String>) {
+        //val newPermissionStr = arrayOfNulls<String>(permissionsList.size)
+        val newPermissionStr = Array(permissionsList.size) { "" }
+        for (i in newPermissionStr.indices) {
+            newPermissionStr[i] = permissionsList[i]
+        }
+        if (newPermissionStr.isNotEmpty()) {
+            permissionsLauncher.launch(newPermissionStr)
+        }
+    }
+
+    private fun checkPermissionCamera() {
+        when {
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+              //  optionTakingImage()
+                cameraGranted = true
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) -> {
+                CommonMethods.showMessage(this, getString(R.string.permission_required))
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted)
+                cameraGranted = true
+                //optionTakingImage()
+            else
+                CommonMethods.showMessage(this, getString(R.string.permission_required))
+        }
 }
